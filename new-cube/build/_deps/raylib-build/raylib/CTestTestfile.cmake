5044f886a3d0de46072158e7fd2b58db5acbd7dd
Larkstongueinaspic <1252024992@qq.com>
2025-11-10 11:14:05 +0800

    Delete new-cube/build directory

--- new-cube/build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bo_yu/Documents/bupt/l_linux/linux-3d-cube/new-cube/build/_deps/raylib-src/src
-# Build directory: /Users/bo_yu/Documents/bupt/l_linux/linux-3d-cube/new-cube/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
